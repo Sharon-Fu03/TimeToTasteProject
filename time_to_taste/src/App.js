0f82da3173e665e4de4b96e6f1258ac5ebed63db
Sharon-Fu03 <s1101742@mail.yzu.edu.tw>
2025-09-04 14:37:37 +0800

    連結資料庫

--- time_to_taste/src/App.js
+++ time_to_taste/src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [message, setMessage] = useState('');
+  const testBackend = async () => {
+        try {
+          const response = await axios.get('/hello');
+          setMessage(response.data);
+        } catch (error) {
+          console.error('API 呼叫失敗:', error);
+          setMessage('連接後端失敗');
+        }
+      };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <p>雞胸肉</p>
+        <p>雞胸肉的營養成分如下：</p>
+     <button onClick={testBackend} style={{margin: '10px', padding: '10px'}}>
+          測試後端 API
+        </button>
+
+{message && <p style={{color: 'lightgreen'}}>{message}</p>}
+
       </header>
     </div>
   );
